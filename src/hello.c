84232148ed1173c95533b21586fdff630c18a7d5
Adrian Ko <adrianchko@gmail.com>
2015-07-01 13:29:21 +0100

    Add for loop

--- src/hello.c
+++ src/hello.c
@@ -8,9 +8,18 @@ double div(int a, int b) {
     return a / (double) b;
 }
 
+void loops() {
+    printf("for loop:\n");
+    
+    for (int i = 0; i < 5; i++) {
+        printf("for: %d\n", i);
+    }
+}
+
 int main() {
     hello();
     printf("Div 95 / 31: %.10f\n", div(95, 31));
+    loops();
     
     return 0;
 }
