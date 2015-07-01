4e08bf135775465128486f9d715c3a0e9aba4687
Adrian Ko <adrianchko@gmail.com>
2015-07-01 13:29:02 +0100

    Add div function

--- src/hello.c
+++ src/hello.c
@@ -4,8 +4,13 @@ void hello() {
     printf("Hello, world\n");
 }
 
+double div(int a, int b) {
+    return a / (double) b;
+}
+
 int main() {
     hello();
+    printf("Div 95 / 31: %.10f\n", div(95, 31));
     
     return 0;
 }
