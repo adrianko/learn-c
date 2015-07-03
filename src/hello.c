aa6fbbdad4537c65515d1dbd1c154aa7933f1b39
Adrian Ko <adrianchko@gmail.com>
2015-07-03 11:34:29 +0100

    Add struct pointer access

--- src/hello.c
+++ src/hello.c
@@ -82,6 +82,10 @@ void structures() {
     point1.y = 4;
     printf("point1:\n");
     printf("x: %d, y: %d\n", point1.x, point1.y);
+    printf("\n");
+    struct point *ppoint1 = &point1;
+    printf("point1 pointer:\n");
+    printf("x: %d, y: %d\n", ppoint1->x, ppoint1->y);
 }
 
 void lb() {
