bedd2a2c61f1c4f994b3abe84c9ddafa2dda672b
adrianko <adrianchko@gmail.com>
2015-07-05 18:09:48 +0100

    Atom formatting

--- src/hello.c
+++ src/hello.c
@@ -15,14 +15,14 @@ void functions() {
 
 void loops() {
     printf("for loop:\n");
-    
+
     for (int i = 0; i < 5; i++) {
         printf("for: %d\n", i);
     }
-    
+
     printf("while loop:\n");
     int j = 0;
-    
+
     while (j < 5) {
         printf("while: %d\n", j++);
     }
@@ -31,16 +31,16 @@ void loops() {
 void conditions() {
     printf("if/else stmt:\n");
     int f = 0;
-    
+
     if (f == 1) {
         printf("f is 1\n");
     } else {
         printf("f: %d\n", f);
     }
-    
+
     printf("switch/case stmt:\n");
     int g = 5;
-    
+
     switch (g) {
         case 0:
             printf("g is 0\n");
@@ -113,8 +113,8 @@ int main() {
     pointers();
     lb();
     structures();
-	lb();
-	strings();
-    
+	  lb();
+	  strings();
+
     return 0;
 }
