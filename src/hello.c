053cb96626eb9a9802c2fc1dcbe58d4bbf2455a3
Adrian Ko <adrianchko@gmail.com>
2015-07-07 14:10:34 +0100

    Add header file

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "hello.h"
 
 void hello() {
     printf("Hello, world\n");
---
+++ src/hello.h
@@ -0,0 +1,12 @@
+void hello();
+double divide(int a, int b);
+void functions();
+void loops();
+void conditions();
+void buffer_overflow();
+void add(int *x, int add);
+void pointers();
+void structures();
+void strings();
+void memory_allocator();
+void lb();
