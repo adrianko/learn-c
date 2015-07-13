030e96a7ef0873b16a8621432147e743793300c5
Adrian Ko <adrianchko@gmail.com>
2015-07-13 10:47:50 +0100

    create reverse linked list of ints

--- src/linked_list.c
+++ src/linked_list.c
@@ -2,11 +2,27 @@
 #include <stdio.h>
 
 struct node {
-    int x;
+    int val;
     struct node *next;
 };
 
 int main() {
-    
+    struct node *curr, *head;
+    head = NULL;
+
+    for (int i = 1; i < 10; i++) {
+        curr = (struct node *) malloc(sizeof(struct node));
+        curr->val = i;
+        curr->next = head;
+        head = curr;
+    }
+
+    curr = head;
+
+    while (curr) {
+        printf("%d\n", curr->val);
+        curr = curr->next;
+    }
+
     return 0;
 }
